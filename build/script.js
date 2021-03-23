5223b9a04965f99d32c5f0491102cd3047598105
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-03-23 15:13:16 +0300

    add delete func

--- build/script.js
+++ build/script.js
@@ -1,11 +1,27 @@
 let btn = document.getElementById("btn");
 let urlPng = document.getElementById("Url_png");
+let textPng = document.getElementById("textPng");
 
 btn.addEventListener("click", function () {
     let text_img = document.createElement("p");
     let new_img = document.createElement("img");
-    let tmp = urlPng.value();
-    text_img.setAttribute("src", tmp);
-    arena.append(text_img)
-    arena.append(new_img)
+    let new_div = document.createElement("div");
+    let close = document.createElement('span');
+    let tmp_img = urlPng.value;
+    let tmp_desc = textPng.value;
+
+    new_img.setAttribute("src", tmp_img);
+    new_img.setAttribute("width", "300");
+    new_img.setAttribute("height", "300");
+    text_img.innerHTML = tmp_desc;
+    close.textContent = 'X';
+    close.addEventListener("click", function (e) {
+        e.target.parentElement.remove()
+    })
+    new_div.append(text_img)
+    new_div.append(new_img)
+    new_div.append(close)
+
+    arena.append(new_div)
 });
+
